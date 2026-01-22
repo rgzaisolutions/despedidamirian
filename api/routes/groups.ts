6f44444685b24a0eeacfaec1cf853474adbca954
RGZ AI Solutions <rgzaisolutions@gmail.com>
2026-01-22 16:48:26 +0100

    Eliminando todo el contenido por petición del usuario

--- api/routes/groups.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { joinGroup, createGroup } from '../controllers/groupController.js';
-
-const router = Router();
-
-router.post('/join', joinGroup);
-router.post('/create', createGroup);
-
-export default router;
